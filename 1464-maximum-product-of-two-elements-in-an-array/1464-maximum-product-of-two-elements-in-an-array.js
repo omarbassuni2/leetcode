6ceb3b331dae4808e880373c60b0e7d677a8b73f
Omar Bassuni <omarbassuni2@gmail.com>
2022-05-31 18:57:50 +0200

    Time: 119 ms (12.41%), Space: 44.2 MB (15.46%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    nums = nums.sort((a, b) => {
+        return b-a;
+    });
+    return (nums[0] - 1) * (nums[1] - 1);
+};
