33de82268f7b7bda74a654ca6751ebd9e7601b19
Omar Bassuni <omarbassuni2@gmail.com>
2022-06-20 23:41:47 +0200

    Time: 111 ms (76.41%), Space: 50.8 MB (95.77%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  k %= nums.length;
+  nums.unshift(...nums.splice(nums.length - k));
+  return nums;
+};
