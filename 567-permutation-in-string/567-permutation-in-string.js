31e6a097a22ebbc845474dda0be7d7b5cbc23bfc
Omar Bassuni <omarbassuni2@gmail.com>
2022-06-26 23:58:54 +0100

    Time: 7828 ms (5.06%), Space: 51.4 MB (14.07%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    const hashMap = new Map();
+    for(var i = 0; i < s1.length; i += 1){
+        let current = hashMap.get(s1[i]) || 0;
+        hashMap.set(s1[i], current + 1);
+    }
+    let mapSize = s1.length;
+    for(var i = 0; i < s2.length; i += 1){
+        if(hashMap.get(s2[i])){
+            let tempMap = new Map(hashMap);
+            for(var j = i; j < i + mapSize; j += 1){
+                if(tempMap.get(s2[j]) && tempMap.get(s2[j]) > 0){
+                    let current = tempMap.get(s2[j]);
+                    tempMap.set(s2[j], current - 1);
+                }else   break;
+            }
+            let output = true;
+            for(const [key, value] of tempMap){
+                if(value != 0)  output = false;
+            }
+            if(output)  return output;
+        }
+    }
+    return false;
+};
