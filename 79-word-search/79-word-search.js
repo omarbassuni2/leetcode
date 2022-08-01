66cedda1d5cf406210df6f3bfa8c07701272f1d3
Omar Bassuni <omarbassuni2@gmail.com>
2022-08-01 23:44:08 +0200

    Time: 4556 ms (6.23%), Space: 48.5 MB (38.97%) - LeetHub

---
+++ 79-word-search/79-word-search.js
@@ -0,0 +1,25 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    const visited = new Set();
+    function backtracking(row, col, pointer){
+        if(pointer == word.length)  return true;
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || 
+           board[row][col] != word[pointer] || visited.has([row, col].toString()))     return false;
+        visited.add([row, col].toString());
+        const isExist = (backtracking(row, col + 1, pointer + 1) || backtracking(row, col - 1, pointer + 1) ||
+               backtracking(row + 1, col, pointer + 1) ||  backtracking(row - 1, col, pointer + 1));
+        visited.delete([row, col].toString());
+        return isExist;
+    }
+    
+    for(var i = 0; i < board.length; i += 1){
+        for(var j = 0; j < board[0].length; j += 1){
+            if(backtracking(i, j, 0))   return true;
+        }
+    }
+    return false;
+};
