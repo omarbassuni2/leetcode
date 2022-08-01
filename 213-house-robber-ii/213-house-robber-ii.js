c02925fa1e0f860369d1e31b027bbc7d27fb7bdd
Omar Bassuni <omarbassuni2@gmail.com>
2022-08-02 01:56:08 +0200

    Time: 64 ms (91.20%), Space: 41.8 MB (82.97%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if(nums.length === 1) return nums[0]
+    const helper = (houses , start , end) => {
+      let max1 = 0, max2 = 0 , temp;
+     for(let i = start; i < houses.length - end ; i++){
+      temp = Math.max(houses[i] + max1 , max2)
+      max1 = max2
+      max2 = temp
+     }
+     return max2
+    }
+    return Math.max(helper(nums , 0 , 1) , helper(nums , 1 , 0))
+};
