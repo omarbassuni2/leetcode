549b2d40b305e0f9f3dd267393a58029a9d1950c
Omar Bassuni <omarbassuni2@gmail.com>
2022-08-03 20:43:36 +0200

    Time: 204 ms (66.44%), Space: 43.6 MB (80.07%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    const L = new Array(nums.length).fill(1)
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i] && L[j] + 1 > L[i]) {
+                L[i] = L[j] + 1
+            }
+        }
+    }
+    return Math.max(...L);
+};
