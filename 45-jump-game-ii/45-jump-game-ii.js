0d2bd4bb6f6f58bb0263133e2a693f90c0c6b939
Omar Bassuni <omarbassuni2@gmail.com>
2022-08-11 01:13:51 +0200

    Time: 271 ms (24.77%), Space: 45 MB (22.03%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.js
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -3,12 +3,17 @@
  * @return {number}
  */
 var jump = function(nums) {
-    const memoization = new Array(nums.length).fill(Infinity);
-    memoization[nums.length - 1] = 0;
-    for(var i = nums.length - 2; i >= 0 ; i -= 1){
-        for(var jump = 1; jump <= nums[i] && i + jump < nums.length; jump += 1){
-            memoization[i] = Math.min(memoization[i], memoization[i + jump] + 1);
+    if(nums.length == 1)    return 0;
+    const dp = new Array(nums.length).fill(Infinity);
+    dp[nums.length - 1] = 0;     //dp[4] = 0
+    dp[nums.length - 2] = 1;    //1 + dp[4] = 1 
+    for(var i = nums.length - 3; i >= 0; i -= 1){
+        if(nums[i] === 0)    continue;
+        let currentVal = 1 + dp[i + 1];
+        for(var j = 2; j <= nums[i] && i + j < nums.length; j += 1){
+            currentVal = Math.min(currentVal, 1 + dp[i + j]); //dp[1]=Min( (1 + dp[2]), (1+dp[3]),(1+dp[4]) )we add the one because we account for the move
         }
+        dp[i] = currentVal;
     }
-    return memoization[0];
+    return dp[0];
 };
