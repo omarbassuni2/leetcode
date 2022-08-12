928bdefac0ae1809996a93e9b0e04eabad022236
Omar Bassuni <omarbassuni2@gmail.com>
2022-08-12 22:16:09 +0200

    Time: 131 ms (70.21%), Space: 52.3 MB (82.56%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -4,19 +4,15 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-	let dp = new Array(text1.length + 1)
-	for (let i = 0; i < dp.length; i++) {
-		dp[i] = new Array(text2.length + 1).fill(0)
-	}
-
-	for (let i = 1; i < dp.length; i++) {
-		for (let j = 1; j < dp[0].length; j++) {
-			if (text1[i-1] === text2[j-1]) {
-				dp[i][j] = 1 + dp[i-1][j-1]
-			} else {
-				dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
-			}
-		}
-	}
-	return dp[text1.length][text2.length]
+    const matrix = new Array(text1.length + 1).fill(0);
+    matrix.forEach((ele, i) => {
+        matrix[i] = new Array(text2.length + 1).fill(0);
+    })
+    for(var i = 1;i < matrix.length; i += 1){
+        for(var j = 1; j < matrix[0].length; j += 1){
+            if(text1[i - 1] == text2[j - 1])    matrix[i][j] = 1 + matrix[i - 1][j - 1];
+            else    matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
+        }
+    }
+    return matrix[text1.length][text2.length];
 };
