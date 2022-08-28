d0a989f9ad42ec4521b95fd28fdfb8666ed8d431
Omar Bassuni <omarbassuni2@gmail.com>
2022-08-28 19:14:24 +0100

    Time: 93 ms (41.36%), Space: 41.9 MB (72.58%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -3,18 +3,24 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let left = 0, right = nums.length - 1;
-    let mid = Math.ceil((left + right) / 2);
-    let leastNum = nums[mid];
+    /*
+    [3,4,5,1,2], -- 5 --, left is sorted, min is 3, go check the right arr
+    [1,2]
+    
+    [4,5,6,7,0,1,2], -- 7--, left is sorted, min is 4, check right
+    [0, 1, 2], -- 1 --, left is sorted, min is 0, check right 
+    [2], -- 2 --, reached base case, min is 0;
+    */
+    let min = Infinity, left = 0, right = nums.length - 1;
     while(left <= right){
-        if(nums[left] <= nums[mid]){ //left part is sorted & less than our mid 
-            leastNum = Math.min(leastNum, nums[left]);
+        const mid = Math.floor((left + right) / 2);
+        if(nums[left] <= nums[mid]){    //left is sorted
+            min = Math.min(min, nums[left]);
             left = mid + 1;
         }else{
-            leastNum = Math.min(leastNum, nums[mid]);
+            min = Math.min(min, nums[mid]) //might need to remove the + 1
             right = mid - 1;
         }
-        mid = Math.ceil((left + right) / 2);
     }
-    return leastNum;
+    return min;
 };
