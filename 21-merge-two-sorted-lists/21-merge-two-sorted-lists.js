ee2e39586a6ddcca9ff0a9bda5f8fb17128032eb
Omar Bassuni <omarbassuni2@gmail.com>
2022-08-28 19:37:44 +0100

    Time: 108 ms (41.71%), Space: 43.7 MB (95.46%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,9 +11,9 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if(list1 == null) return list2;
-    if(list2 == null) return list1;
-    if(list1.val < list2.val){
+    if(!list1)  return list2;
+    if(!list2)  return list1;
+    if(list1.val <= list2.val){
         list1.next = mergeTwoLists(list1.next, list2);
         return list1
     }else{
