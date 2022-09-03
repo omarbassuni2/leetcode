685bf10fed201496e8e7bda58811873dd8a1cdc2
Omar Bassuni <omarbassuni2@gmail.com>
2022-09-03 03:17:54 +0200

    Time: 1634 ms (5.01%), Space: 50.4 MB (15.31%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -13,25 +13,20 @@ var mergeKLists = function(lists) {
     let root = new ListNode(0, null);
     const output = root;
     let isListsContainVal = true;
-    let removedCounter = 0;
-    lists.forEach((ele, i) =>{
-        if(ele == null){
-            lists = [...lists.slice(0, i - removedCounter), ...lists.slice(i + 1 - removedCounter)];
-            removedCounter += 1;
-        }
-    });
     while(lists.length){
+        let removedCounter = 0;
+        lists.forEach((ele, i) =>{
+         if(ele == null){
+             lists = [...lists.slice(0, i - removedCounter), ...lists.slice(i + 1 - removedCounter)];
+                removedCounter += 1;
+         }
+        });
+        if(lists.length == 0)   break;
         lists = lists.sort((a,b) => { return a.val-b.val});
         root.next = lists[0];
         root = root.next;
         lists[0] = lists[0]?.next;
         removedCounter = 0;
-        for(var i = 0; i < lists.length; i += 1){
-            if(lists[i] == null){
-                lists = [...lists.slice(0, i  - removedCounter), ...lists.slice(i + 1  - removedCounter)];
-                removedCounter += 1;
-            }
-        }
     }
     return output.next;
 };
