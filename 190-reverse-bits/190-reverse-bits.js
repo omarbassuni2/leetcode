eebf72339d27628ab9244f0795b261518a0523da
Omar Bassuni <omarbassuni2@gmail.com>
2022-09-21 00:04:43 +0200

    Time: 80 ms (83.53%), Space: 43.7 MB (84.68%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.js
+++ 190-reverse-bits/190-reverse-bits.js
@@ -3,8 +3,20 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-    let l = Math.clz32(n);
-    let l2 = n.toString(2).length;
-    let m = n.toString(2).padStart(l+l2,0).split("").reverse().join("");
-    return parseInt(m, 2);
+    let output = 0;
+    for(var i = 0; i < 32; i += 1){
+        output <<= 1;
+        output |= n & 1;
+        n >>>= 1;
+    }
+    return output >>> 0;
 };
+
+/*
+Since we know that it should be 32 bits, let's create a loop with 32 iterations.
+r <<= 1 provide a place for the next bit, e.g. 1 << 1 = 10, 10 << 1 = 100, and so on.
+r |= n & 1 dicide if last bit is 0 or 1
+n >>>= 1 discard right most bit of provided number since it was applied
+
+Last line return r >>> 0 just converts resulted bits into unsigned representation.
+*/
