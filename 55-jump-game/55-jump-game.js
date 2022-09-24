4fae9cf4a09d99985284e0b0d0af1b5803b7067d
Omar Bassuni <omarbassuni2@gmail.com>
2022-09-24 15:59:06 +0200

    Time: 476 ms (25.17%), Space: 47.9 MB (22.02%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -4,14 +4,14 @@
  */
 var canJump = function(nums) {
     if(nums.length == 1)    return true;
-    const output = new Array(nums.length).fill(false);
-    output[nums.length - 1] = true;
-    for(var i =  nums.length - 2; i >= 0; i -= 1){
+    const dp = new Array(nums.length).fill(false);
+    dp[nums.length - 1] = true;
+    for(var i = nums.length - 2; i >= 0; i -= 1){
         if(nums[i] == 0)    continue;
         for(var j = 1; j <= nums[i]; j += 1){
-            output[i] = output[i] || output[i + j];  //dp[1] = dp[2] || dp[3] || dp[4]
-            if(output[i])   break;
+            dp[i] = dp[i] || dp[i + j];
+            if(dp[i])   break;
         }
     }
-    return output[0]
+    return dp[0];
 };
