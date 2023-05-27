bad5437d4abbf8eba6353270c96f4c21e85ef425
Omar Bassuni <omarbassuni2@gmail.com>
2023-05-27 15:45:44 +0300

    Time: 120 ms (83.59%), Space: 52.7 MB (83.32%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -5,7 +5,7 @@
 var groupAnagrams = function(strs) {
     const strsMap = {};
     strs.forEach((str) => {
-        const sortedStr = str.split("").sort();
+        const sortedStr = str.split("").sort().join("");
         strsMap[sortedStr] ? strsMap[sortedStr].push(str) : strsMap[sortedStr] = [str];
     });
     return Object.values(strsMap);
