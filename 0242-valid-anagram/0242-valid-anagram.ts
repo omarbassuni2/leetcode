eb1387896d1d31f402ff3afe7d5a6e34b5f57895
Omar Bassuni <omarbassuni2@gmail.com>
2024-05-16 15:09:11 +0300

    Time: 64 ms (91.17%), Space: 53.6 MB (69.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,18 +1,16 @@
 function isAnagram(s: string, t: string): boolean {
     if(s.length !== t.length) return false;
     const sMap: { [x: string]: number } = {};
-    const tMap: { [x: string]: number } = {};
     s.split("").forEach((char) => {
         if(sMap[char]) sMap[char] +=1;
         else    sMap[char] = 1;
     });
-    t.split("").forEach((char) => {
-        if(tMap[char]) tMap[char] +=1;
-        else    tMap[char] = 1;
-    });
-    if(Object.keys(sMap).length !== Object.keys(tMap).length)    return false;
+    for(let i = 0; i < t.length; i += 1) {
+        if(sMap[t[i]] && sMap[t[i]] > 0) sMap[t[i]] -= 1;
+        else return false;
+    }
     for(const key of Object.keys(sMap)) {
-        if(!tMap[key] || tMap[key] !== sMap[key])   return false;
+        if(sMap[key] !== 0) return false;
     }
     return true;
 };
