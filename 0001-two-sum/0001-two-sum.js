31a5a3ceefcf19ea49d1a420a627e3e928f6f9b4
Omar Bassuni <omarbassuni2@gmail.com>
2024-05-21 04:37:14 +0300

    Time: 71 ms (49.53%), Space: 53.4 MB (5.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const numMap = {};
-    nums.forEach((num, i) => { numMap[num] ? numMap[num].push(i) : numMap[num] = [i] });
-    for (let i = 0; i < nums.length; i += 1) {
-        const remainder = target - nums[i];
-        if (numMap[remainder]) {
-            if (remainder !== nums[i])  return [i, numMap[remainder][0]];
-            else if (remainder === nums[i] && numMap[remainder].length > 1)  return numMap[remainder].slice(0, 2)
-            
+    const map = {};
+    nums.forEach((n, i) => {
+        if(map[n])  map[n].push(i);
+        else map[n] = [i];
+    });
+    for(let i = 0; i < nums.length; i += 1) {
+        const temp = target - nums[i];
+        if(map[temp]) {
+            const secondIndex = map[temp][map[temp].length - 1];
+            if(i !== secondIndex)    return [i, secondIndex];
         }
-    };
+    }
 };
