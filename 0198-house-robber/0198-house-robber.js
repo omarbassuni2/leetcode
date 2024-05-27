b11023107f7af16ebfd8970537fe3ed8fc047dac
Omar Bassuni <omarbassuni2@gmail.com>
2024-05-27 22:26:55 +0300

    Time: 66 ms (5.34%), Space: 48 MB (97.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,19 +3,11 @@
  * @return {number}
  */
 var rob = function(nums) {
-    // Nums:  [2,7,            9,          3, 1]
-    // Dp: Max(2, 2 + 10, 2 + 3, 2 + 1)  Max(7, 7 + 3, 7 + 1)    Max(9, 9 + 1) 3 1
     if(nums.length <= 2)    return Math.max(...nums);
-    const len = nums.length;
-    const dp = new Array(len).fill(0);
-    dp[len - 1] = nums[len - 1], dp[len - 2] = nums[len - 2];
-    let max = Math.max(...dp.slice(len - 2));
-    for(let i = len - 3; i >= 0; i -= 1) {
-        for(let j = i + 2; j < len; j += 1) {
-            dp[i] = Math.max(dp[i], nums[i], dp[j] + nums[i]);
-        }
-        max = Math.max(max, dp[i]);
+    const dp = new Array(nums.length).fill(0);
+    dp[0] = nums[0], dp[1] = Math.max(nums[0], nums[1]);
+    for(let i = 2; i < nums.length; i += 1) {
+        dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
     }
-    console.log(dp)
-    return max;
+    return dp[nums.length - 1];
 };
