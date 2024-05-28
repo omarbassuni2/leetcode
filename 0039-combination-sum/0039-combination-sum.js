e0a4c877834e81a83ac7cf53f08100f6f3248539
Omar Bassuni <omarbassuni2@gmail.com>
2024-05-28 20:26:06 +0300

    Time: 92 ms (15.15%), Space: 57.1 MB (27.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -7,7 +7,7 @@ var combinationSum = function(candidates, target) {
     const output = [];
     function dfs(i, currArr, remainder) {
         if(remainder === 0) {
-            output.push(currArr);
+            output.push(currArr.slice());
             return;
         } else if(i >= candidates.length || remainder < 0) {
             return;
