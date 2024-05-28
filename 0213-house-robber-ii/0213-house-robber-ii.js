f3e1902f83009a977bc6b30e7439682c38a905be
Omar Bassuni <omarbassuni2@gmail.com>
2024-05-28 08:07:22 +0300

    Time: 53 ms (48.42%), Space: 48.5 MB (78.95%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.js
+++ 0213-house-robber-ii/0213-house-robber-ii.js
@@ -6,14 +6,18 @@
 https://leetcode.com/problems/house-robber-ii/discuss/1234674/Javascript-or-DP-or-Time%3A-O(n)-Space%3A-O(1)
 */
 var rob = function(nums) {
-    if (nums.length == 1) return nums[0]
-    let prev1 = 0, cur1 = 0
-    let prev2 = 0, cur2 = 0
-    for (let i = 1; i < nums.length; i++) {
-        const n1 = nums[i-1]
-        const n2 = nums[i]
-        cur1 = Math.max(n1 + prev1, prev1 = cur1)
-        cur2 = Math.max(n2 + prev2, prev2 = cur2)
+    if(nums.length <= 3)    return Math.max(...nums);
+    let max = 0;
+    let dp = new Array(nums.length).fill(0);
+    dp[0] = nums[0], dp[1] = Math.max(nums[0], nums[1]);
+    for(let i = 2; i < nums.length - 1; i += 1) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        max = Math.max(dp[i], max);
     }
-    return Math.max(cur1, cur2)
+    dp[1] = nums[1], dp[2] = Math.max(nums[1], nums[2]);
+    for(let i = 3; i < nums.length; i += 1) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        max = Math.max(dp[i], max);
+    }
+    return max;
 };
