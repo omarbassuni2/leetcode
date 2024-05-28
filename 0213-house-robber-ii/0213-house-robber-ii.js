3f8b7d91e424f270bda20f7f1bfdcd8b173ab020
Omar Bassuni <omarbassuni2@gmail.com>
2024-05-28 08:05:01 +0300

    Time: 49 ms (69.21%), Space: 48.9 MB (36.45%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+/*
+https://leetcode.com/problems/house-robber-ii/discuss/5161078/JavaScraipt-tabulation
+*/
+var rob = function(nums) {
+    if(nums.length <= 3)    return Math.max(...nums);
+    let max = 0;
+    let dp = new Array(nums.length).fill(0);
+    dp[0] = nums[0], dp[1] = Math.max(nums[0], nums[1]);
+    for(let i = 2; i < nums.length - 1; i += 1) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        max = Math.max(dp[i], max);
+    }
+     dp = new Array(nums.length).fill(0);
+    dp[1] = nums[1], dp[2] = Math.max(nums[1], nums[2]);
+    for(let i = 3; i < nums.length; i += 1) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        max = Math.max(dp[i], max);
+    }
+    return max;
+};
