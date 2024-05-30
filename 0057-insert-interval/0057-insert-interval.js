5d9dd9e06ee831d48ad9460ee9e617412af0fa4a
Omar Bassuni <omarbassuni2@gmail.com>
2024-05-30 17:57:39 +0300

    Time: 67 ms (87.69%), Space: 54.7 MB (31.38%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -7,30 +7,27 @@
 
 /*
 intervals = [[1,3],[6,9]], newInterval = [2,5]
-
 intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
             [[1,2] , , ,      ,[12,16]] [3, 10]
-
 intervals = [[6,9]], newInterval = [2,5]
 intervals = [[6,9]], newInterval = [0,0]
 intervals = [[6,9]], newInterval = [6,9]
 intervals = [], newInterval = [6,9]
 */
-function inbound(interval, newInterval) {
-    return (interval[0] <= newInterval[0] && newInterval[0] <= interval[1] || interval[0] <= newInterval[1] && newInterval[1] <= interval[1])
-}
 var insert = function(intervals, newInterval) {
     if(intervals.length === 0)  return [newInterval];
     const output = [];
-    for(const interval of intervals) {
-        if(inbound(interval, newInterval) || inbound(newInterval, interval)) {
-            const minBound = Math.min(interval[0], newInterval[0]), maxBound =   Math.max(interval[1], newInterval[1]);
-            newInterval = [minBound, maxBound];
-         } else {
-             output.push(interval);
-         }
+    //using the fact its a sorted interval (think of it as Binary search)
+    for(let i = 0; i < intervals.length; i += 1) {
+        const curr = intervals[i];
+        if(newInterval[1] < curr[0]) {
+            return [...output, newInterval, ...intervals.slice(i)];
+        } else if(newInterval[0] > curr[1]) {
+            output.push(curr);
+        } else {
+            newInterval = [Math.min(newInterval[0], curr[0]), Math.max(newInterval[1], curr[1])];
+        }
     }
-    let left = 0;
-    while(left < intervals.length && intervals[left][0] < newInterval[0]) left += 1;
-    return [...output.slice(0, left), newInterval, ...output.slice(left)];
+    output.push(newInterval);
+    return output;
 };
