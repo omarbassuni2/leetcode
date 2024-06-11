8cc51e69c90cca8e0344092170357d8980d5e0b4
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-11 21:03:32 +0300

    Time: 47 ms (97.89%), Space: 51.7 MB (38.15%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -11,17 +11,21 @@
  */
 var cloneGraph = function(node) {
     if(!node)   return null;
-    const queue = [node], map = {};
-    map[node.val] = new Node(node.val);
+    // Create Root as a new node
+    const mapOfNodes = { [node.val]: new Node(node.val)}, visited = new Set([node.val]);
+    const queue = [node];
     while(queue.length) {
-        const tmp = queue.shift();
-        for(let i = 0; i < tmp.neighbors.length; i += 1) {
-            if(!map[tmp.neighbors[i].val]) {
-                map[tmp.neighbors[i].val] = new Node(tmp.neighbors[i].val);
-                queue.push(tmp.neighbors[i])
+        const curr = queue.shift();
+        // Create its childern and add them to the map
+        curr.neighbors.forEach((child) => {
+            if(!(child.val in mapOfNodes))  mapOfNodes[child.val] = new Node(child.val);
+        // Make the new root have its new childern
+            mapOfNodes[curr.val].neighbors.push(mapOfNodes[child.val])
+            if(!visited.has(child.val)) {
+                visited.add(child.val)
+                queue.push(child);
             }
-            map[tmp.val].neighbors.push(map[tmp.neighbors[i].val]);
-        }
+        })
     }
-    return map[node.val]
+    return mapOfNodes[node.val];
 };
