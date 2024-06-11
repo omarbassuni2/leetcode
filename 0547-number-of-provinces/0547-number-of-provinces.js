ea2b3ed9113350ecd5b81ae7b232afac147618eb
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-12 00:39:42 +0300

    Time: 95 ms (6.96%), Space: 53.5 MB (23.48%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ 
+ [[1,1,0],
+  [1,1,0],
+  [0,0,1]]
+ */
+
+var findCircleNum = function(isConnected) {
+    let parent = new Array(isConnected.length).fill(0).map((_, i) => i);
+    function find(x) {
+        if (parent[x] !== x) {
+            parent[x] = find(parent[x]); // Path compression
+        }
+        return parent[x];
+    };
+    
+    function union(x, y) {
+        const xParent = find(x);
+        const yParent = find(y);
+        if (xParent !== yParent) {
+            parent[xParent] = yParent; // Union
+        }
+    };
+    
+    for (let i = 0; i < isConnected.length; i += 1) {
+        for (let j = 0; j < isConnected[0].length; j += 1) {
+            if (i !== j && !!isConnected[i][j]) {
+                union(i, j);
+            }
+        }
+    }
+    
+    return new Set(parent.map(find)).size;
+};
