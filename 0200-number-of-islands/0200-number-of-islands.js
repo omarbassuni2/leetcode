5653ffbe02f5c2e744ffe894754b98406c45fb40
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-13 19:35:07 +0300

    Time: 147 ms (6.55%), Space: 63.3 MB (9.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -2,47 +2,25 @@
  * @param {character[][]} grid
  * @return {number}
  */
-/*
-Input: grid = [["1"]]
-Input: grid = [["0"]]
-Input: grid = [
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
-]
-Input: grid = [
-  ["1","0","0","1","0"],
-  ["0","0","0","0","0"],
-  ["0","1","0","0","0"],
-  ["0","0","0","0","0"]
-]
-
-[
-["1","1","1"],
-["0","1","0"],
-["1","1","1"]]
-*/
 var numIslands = function(grid) {
-    const visited = new Set();
-    let counter = 0;
+    const visited =  new Set();
+    let numOfIslands = 0;
     function dfs(i, j) {
-        visited.add([i, j].toString());
-        for(const [ii, jj] of [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
-            const r = i + ii, c = j + jj;
-            if (!(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || visited.has([r, c].toString()))) {
-                    if(grid[r][c] === '1')  dfs(r, c);
-            }
-        }
-        
+        if(i < 0 || j < 0 || i >= grid.length || 
+           j >= grid[0].length || visited.has([i,j].join(",")) || grid[i][j] === '0') return;
+        visited.add([i,j].join(",")) 
+        dfs(i - 1, j);
+        dfs(i + 1, j);
+        dfs(i, j - 1);
+        dfs(i, j + 1);
     }
     for(let i = 0; i < grid.length; i += 1) {
         for(let j = 0; j < grid[0].length; j += 1) {
-            if(grid[i][j] === '1' && !visited.has([i, j].toString())) {
-                dfs(i, j);
-                counter += 1;
+            if(grid[i][j] === '1' && !visited.has([i,j].join(",")))  {
+                dfs(i,j);
+                numOfIslands += 1;
             }
         }
     }
-    return counter;
+    return numOfIslands;
 };
