08c036713e3b5ea11ba908ecd5008d1e312d6371
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-13 21:09:09 +0300

    Time: 73 ms (44.70%), Space: 55.4 MB (22.03%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ 
+ [[2,1,1],[1,1,0],[0,1,1]]
+ */
+var orangesRotting = function(grid) {
+    let time = 0, fresh = 0;
+    const queue = [];
+    for(let i = 0; i < grid.length; i += 1) {
+        for(let j = 0; j < grid[0].length; j += 1) {
+            if(grid[i][j] === 1)   fresh += 1;
+            if(grid[i][j] === 2)   queue.push([i, j]);
+        }
+    }
+    while(queue.length && fresh > 0) {
+        const size = queue.length;
+        for(let k = 0; k < size; k += 1) {
+           const [i, j] = queue.shift();
+            for(const [r, c] of [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]) {
+                if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] !== 1)   continue;
+                grid[r][c] = 2;
+                queue.push([r, c]);
+                fresh -= 1;
+            }
+        }
+        time += 1;
+    }
+    return fresh === 0 ? time : -1;
+}
+
+
