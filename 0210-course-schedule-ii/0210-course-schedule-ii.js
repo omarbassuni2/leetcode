0f0428364537c81f649e1c90054c3735735e3605
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-15 20:33:48 +0300

    Time: 81 ms (39.18%), Space: 55.3 MB (67.93%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -5,21 +5,30 @@
  */
 var findOrder = function(numCourses, prerequisites) {
     if(numCourses === 1)    return [0];
-    const output = [], cycle = new Set(), visited = new Set(), map = {};
+    const output = [], cycle = new Set(), map = {};
+    // Create an adjancey list
     for(let i = 0; i < numCourses; i += 1)  map[i]  = [];
     for(const [course, pre] of prerequisites)  map[course].push(pre);
+    
+    // Create a dfs 
     function dfs(course) {
-        if(cycle.has(course))   return false;
-        if(visited.has(course)) return true;
+        // Check if the current path has cycle
+        if(cycle.has(course))   return false;       
+        // Check if the course has no dependecy or check its dependencies and it can take them all
+        if(!(course in map)) return true;           
         cycle.add(course);
         for(const pre of map[course]) {
             if(!dfs(pre))   return false;
         }
         cycle.delete(course);
-        visited.add(course);
-        output.push(course);
+        // Delete the course from map to mark it as can be taken with all its dependencies
+        delete map[course];     
+        // Put the courses in its right order
+        output.push(course);       
         return true;
     }
+    
+    // Check 
     for(let i = 0; i < numCourses; i += 1) {
         if(!dfs(i))    return [];
     }
