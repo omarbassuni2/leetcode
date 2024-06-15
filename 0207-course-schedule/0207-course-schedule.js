11509216ae8253fd5a3e0e69b4ccd9b2691116e2
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-15 20:00:19 +0300

    Time: 57 ms (93.95%), Space: 55.5 MB (45.46%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -6,22 +6,26 @@
  [[1,0],[0,1]] {0 :(1), 1: (0)}
  */
 var canFinish = function(numCourses, prerequisites) {
-    const visited = new Set(), preMap = {};
+    const cycle = new Set(), preMap = {};
+    // Create adjacency list
     for(const [course, pre] of prerequisites) {
         if(course in preMap)    preMap[course].push(pre);
         else                    preMap[course] = [pre];
     }
     function dfs(crs) {
-        if(visited.has(crs))    return false;
-        if(!(crs in preMap))    return true;
-        visited.add(crs);
+        if(cycle.has(crs))    return false; // Means two courses are co-dependant 
+        if(!(crs in preMap))    return true;    // Means course has no prerequisite 
+        // Adding to the visited set to detect cycles along the current path
+        cycle.add(crs);
         for(const pre of preMap[crs]) {
             if(!(dfs(pre))) return false;
         }
-        visited.delete(crs);
-        delete preMap[crs];
+         // removing from the visited set to consider new paths
+        cycle.delete(crs);
+        delete preMap[crs];     // To denote that this course has no cycles in its path and can be taught
         return true
     }
+    // Iterating over courses that have prerequisites and running DFS on them
     for(const crs of Object.keys(preMap)) {
         if(!(dfs(crs)))     return false;
     }
