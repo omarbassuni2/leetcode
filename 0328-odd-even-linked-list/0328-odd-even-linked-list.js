8e62bac11e40af7b709fe3404e3f46b9904b9efb
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-16 21:19:33 +0300

    Time: 76 ms (18.31%), Space: 52.9 MB (73.91%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.js
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -10,6 +10,13 @@
  * @return {ListNode}
  */
 var oddEvenList = function(head) {
+    /*
+        The idea is to keep the even head, and make the odd head is the last odd node.
+        make odd next point to even next, then advance odd. Now odd's next is an even node,
+        so we make even next = new odd next and then advance the even.
+        Now after we made all evens and odds connected to each other, we need to connect them together,
+        so we make the last odd head.next point to the evenHead
+    */
     if(!head || !head.next) return head;
     let odd = head, even = head.next;
     const evenHead = head.next;
