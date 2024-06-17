3b015cee71edc5d03129658a5bb99aa4814cdf3c
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-17 15:06:37 +0300

    Time: 62 ms (9.94%), Space: 49.1 MB (27.49%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -10,10 +10,18 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
+        /*
+    The idea is to use 3 pointers. Left and right to swap the two node. and prevPair to update links between pairs.
+    
+    The trick is using prevPair the right way. 
+    
+    After swapping nodes, we make prevPair.next point to the first node
+    and prevPair point to the second node, so in the next iteration prevPair can point to the swapped node of the next pair
+    */
     if(!head)     return head;
     const dummy = new ListNode(0, head);
-    let prev = dummy, left = head, right = head.next;
-    while(left && right) {
+    let prevPair = dummy, left = head, right = left.next;
+    while(left && left.next) {
         // Save next pairs
         const nextPair = right.next;
         
@@ -22,12 +30,12 @@ var swapPairs = function(head) {
         right.next = left;
         left.next = oldRightNext;
         
-        // prev.next should be pointing to the new left
-        prev.next = right
+        // prevPair.next should be pointing to the new left
+        prevPair.next = right
         
         // advance pointers 
-        prev = left
         if(!nextPair)   break;
+        prevPair = left
         left = nextPair;
         right = nextPair.next;
     }
