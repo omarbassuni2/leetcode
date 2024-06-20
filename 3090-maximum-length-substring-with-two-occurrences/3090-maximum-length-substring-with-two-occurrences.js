b3c62ca2b59a36191aa7efc1064002d0f8f28f13
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-20 06:49:45 +0300

    Time: 71 ms (55.19%), Space: 52.5 MB (43.57%) - LeetHub

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maximumLengthSubstring = function(s) {
+    /*
+    The idea is have a left and right indexes. And have a map with char and       
+    its frequency when map[right] exceeds the constraint frequency, we use a       
+    while loop starting from left until the char at map[right] is equal to 2 again
+    */
+    let left = 0, max = 0;
+    const counterMap = { [s[left]]: 1 };
+    for(let right = 1; right < s.length; right += 1) {
+        if(!(s[right] in counterMap))  counterMap[s[right]] = 0;
+        counterMap[s[right]] += 1;
+        while(counterMap[s[right]] > 2) {
+            counterMap[s[left]] -= 1;
+            left += 1;
+        }
+        max = Math.max(max, right - left + 1);
+    }
+    return max;
+};
