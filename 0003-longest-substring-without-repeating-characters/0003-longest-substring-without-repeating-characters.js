aee464c1e15e970d6ecaca738b0867ab4eb5a9bc
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-20 06:49:17 +0300

    Time: 109 ms (27.42%), Space: 54.2 MB (51.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -11,8 +11,8 @@
 var lengthOfLongestSubstring = function(s) {
     if(s.length < 2)   return s.length;
     let max = 0, left = 0;
-    const map = { [s[left]] : 1};
-    for(let right = 1; right < s.length; right += 1) {
+    const map = {};
+    for(let right = 0; right < s.length; right += 1) {
         if(!(s[right] in map)) map[s[right]] = 0;
         map[s[right]] += 1;
         while(map[s[right]] > 1) {
