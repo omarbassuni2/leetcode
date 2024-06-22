95cf835dd789ff5d405f09fbaa1484945b103c73
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-23 02:00:16 +0300

    Time: 82 ms (83.78%), Space: 59.3 MB (45.94%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.js
@@ -0,0 +1,49 @@
+/*
+    The idea is to have two stacks, a normal stack and a minStack that stores min values up until that point. And when you want to
+    getMin you just return that min from the minStack
+*/
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    if(this.minStack.length) this.minStack.push(Math.min(val, this.minStack[this.minStack.length - 1]))
+    else                     this.minStack.push(val);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop();
+    this.minStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
