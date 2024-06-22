8e8d1a7f0a56e5aaece8f12d2396b667009df5d1
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-23 01:21:09 +0300

    Time: 62 ms (64.64%), Space: 51.3 MB (61.68%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.js
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -2,11 +2,14 @@
  * @param {number[]} asteroids
  * @return {number[]}
  */
+/*
+    The trick is to use a Monotic stack. Its conditions should be the element to be pushed must be negative and the last element in the     stack has to be positive, in order to collide. When these conditions are met, we check for absolute value and which one is bigger       and update toBePushed accordingly
+*/
 var asteroidCollision = function(asteroids) {
     const stack = [];
     for(let i = 0; i  < asteroids.length; i += 1) {
         let toBePushed = asteroids[i];
-        while(stack.length && (stack[stack.length - 1] * toBePushed) < 0 && toBePushed < 0) {
+        while(stack.length && toBePushed < 0 && 0 < stack[stack.length - 1]) {
             const popped = stack.pop(), first = Math.abs(popped), second = Math.abs(toBePushed);
             if(first === second) toBePushed =  undefined;
             else {
