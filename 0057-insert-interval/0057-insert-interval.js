39d86f1df415c44d45f2ea46b8342317785c83d1
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-23 17:26:38 +0300

    Time: 74 ms (62.04%), Space: 54.3 MB (48.07%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -7,24 +7,17 @@
 
 /*
 intervals = [[1,3],[6,9]], newInterval = [2,5]
-intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
-            [[1,2] , , ,      ,[12,16]] [3, 10]
-intervals = [[6,9]], newInterval = [2,5]
-intervals = [[6,9]], newInterval = [0,0]
-intervals = [[6,9]], newInterval = [6,9]
-intervals = [], newInterval = [6,9]
+
 */
 var insert = function(intervals, newInterval) {
     if(intervals.length === 0)  return [newInterval];
     const output = [];
-    //using the fact its a sorted interval (think of it as Binary search)
     for(let i = 0; i < intervals.length; i += 1) {
         const curr = intervals[i];
-        if(newInterval[1] < curr[0]) {
+        if(newInterval[0] > curr[1])    output.push(curr);
+        else if (newInterval[1] < curr[0]) {
             return [...output, newInterval, ...intervals.slice(i)];
-        } else if(newInterval[0] > curr[1]) {
-            output.push(curr);
-        } else {
+        }  else {
             newInterval = [Math.min(newInterval[0], curr[0]), Math.max(newInterval[1], curr[1])];
         }
     }
