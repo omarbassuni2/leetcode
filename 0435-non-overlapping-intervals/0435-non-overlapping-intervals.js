561e74bc4949fdea23df27182229214f775431a0
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-23 18:03:10 +0300

    Time: 205 ms (48.98%), Space: 81.3 MB (19.36%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -3,24 +3,28 @@
  * @return {number}
  */
 /*
-intervals = [[1,2]] => 0 
 
 intervals = [[1,2],[2,3],[3,4],[1,3]]
 sorted    = [[1,2],[1,3],[2,3],[3,4]]
 
-intervals = [[1,2],[1,2],[1,2]]
-intervals = [[1,2],[2,3]]
+[[1,2],[1,2],[1,2]]
 
+[[1,2],[2,3]]
+
+    The idea is to sort numbers and check if curr has a lower bound lower than previous upper bound, if so we should remove curr
+    or just increase the count and not updating prev. else we update prev = intervals[i]
+    If prev[0]
 */
 var eraseOverlapIntervals = function(intervals) {
     if(intervals.length === 1)  return 0;
     intervals = intervals.sort((a,b) => a[0]-b[0])
-    let counter = 0, left = 0;
+    let counter = 0, prev = intervals[0];
     for(let i = 1; i < intervals.length; i += 1) {
-        if(intervals[i][0] >= intervals[left][1])   left = i;
-        else {
+        if(intervals[i][0] < prev[1]) {
             counter += 1;
-            left = intervals[i][1] <= intervals[left][1] ? i : left;
+            prev = intervals[i][1] <= prev[1] ? intervals[i] : prev;
+        } else {
+            prev = intervals[i];
         }
     }
     return counter;
