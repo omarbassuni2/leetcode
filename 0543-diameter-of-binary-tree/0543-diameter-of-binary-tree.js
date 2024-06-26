c58fcc5f7ec3f2d3b09f13a52e8e9839a24ad26f
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-26 19:08:25 +0300

    Time: 56 ms (91.58%), Space: 57.1 MB (50.00%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -10,15 +10,19 @@
  * @param {TreeNode} root
  * @return {number}
  */
+/*
+    The idea is to calculate the diameter at each node which is left + right, and to have a variable max to get updated at each node to take the
+    biggest max
+*/
 var diameterOfBinaryTree = function(root) {
     let max = -Infinity;
     function dfs(root) {
         if(!root)   return 0;
         const left = dfs(root.left);
         const right = dfs(root.right);
-        max = Math.max(max, left + right + 1);
+        max = Math.max(max, left + right);
         return Math.max(left, right) + 1;
     };
     dfs(root);
-    return max - 1;
+    return max;
 };
