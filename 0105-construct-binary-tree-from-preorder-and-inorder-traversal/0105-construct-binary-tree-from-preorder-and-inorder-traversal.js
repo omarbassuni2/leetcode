d62c9780b06520c80584f5f7dbb63a4defb49687
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-28 04:15:48 +0300

    Time: 125 ms (45.32%), Space: 127.7 MB (12.98%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -14,11 +14,17 @@
  preorder = [3,9,20,15,7],
  inorder  = [9,3,15,20,7]
  */
+/*
+    The trick is to use slicing preorder and inorder arrays the right way. We know that the first node of preorder is the current node aka Root Node,
+    we find its index in the inorder list, before that index is left node array and after it is right array.
+    
+    we build the current node, then assign its left and right using recursion and returning the curr root
+*/
 var buildTree = function(preorder, inorder) {
-    if(!preorder.length || !inorder.length) return null;
-    const root = new TreeNode(preorder[0]);
-    const index = inorder.indexOf(preorder[0]);
-    root.left = buildTree(preorder.slice(1, index + 1), inorder.slice(0, index + 1));
-    root.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1));
+    if(!preorder.length || !inorder.length)     return  null;
+    const root = new TreeNode(preorder[0]), rootIndex = inorder.indexOf(preorder[0]);
+    const leftLen = rootIndex;
+    root.left = buildTree(preorder.slice(1, leftLen + 1), inorder.slice(0, rootIndex + 1));
+    root.right = buildTree(preorder.slice(leftLen + 1), inorder.slice(leftLen + 1));
     return root;
 };
