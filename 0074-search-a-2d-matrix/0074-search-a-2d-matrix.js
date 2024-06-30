653c9f4ae38c7a82f6afb09d828ed503a87a760d
Omar Bassuni <omarbassuni2@gmail.com>
2024-06-30 23:26:29 +0300

    Time: 49 ms (76.88%), Space: 48.9 MB (57.20%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,25 +4,23 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    // Search the first column and get the desired row
-    let top = 0, bottom = matrix.length - 1;
-    while(top <= bottom) {
-        const mid = Math.floor((top + bottom) / 2);
-        if(matrix[mid][0] === target)   return true;
-        else if(matrix[mid][0] < target) {
-            top = mid + 1;
-        } else {
-            bottom = mid - 1;
+    let left = 0, right = matrix[0].length - 1, low = 0, high = matrix.length - 1, targetRow; 
+    while(low <= high){
+        const mid = Math.floor((low + high) / 2);
+        if(matrix[mid][right] >= target && matrix[mid][left] <= target){
+            targetRow = mid;
+            break;
         }
+        else if(matrix[mid][right] < target){
+            low = mid + 1;
+        } else   high = mid - 1;
     }
-    if (bottom < 0 || bottom >= matrix.length || matrix[bottom][0] > target) return false;
-    // Search for the value
-    let left = 0, right = matrix[bottom].length - 1;
-    while(left <= right) {
-        const mid = Math.floor((left + right ) / 2);
-        if(matrix[bottom][mid] === target) return true;
-        if(matrix[bottom][mid] < target)   left = mid + 1;
-        else                            right = mid - 1;
+    if(isNaN(targetRow))  return false;
+    while(left <= right){
+        const mid = Math.floor((left + right) / 2);
+        if(matrix[targetRow][mid] == target)    return true;
+        else if(target > matrix[targetRow][mid])  left = mid + 1;
+        else    right = mid - 1;
     }
     return false;
 };
