83d96aa91ba74c907f5627eb433dfd1385a2b125
Omar Bassuni <omarbassuni2@gmail.com>
2024-07-01 23:02:49 +0300

    Time: 64 ms (88.04%), Space: 56.8 MB (96.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,11 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
+/*
+    The idea is to keep 2 variables max, currSum. The currSum should be the max between currSum + currNum and currNum alone; applying sliding window method
+    and max should be the max between max and currSum
+*/
 var maxSubArray = function(nums) {
-    let maxSum = nums[0], currSum = 0;
-    nums.forEach((n) => {
-        currSum = Math.max(n + currSum, n);
-        maxSum = Math.max(maxSum, currSum);
+    let max = -Infinity, currSum = 0;
+    nums.forEach((currNum) => {
+        currSum = Math.max(currSum + currNum, currNum);
+        max = Math.max(max, currSum);
     });
-    return maxSum;
+    return max;
 };
