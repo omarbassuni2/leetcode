7fd592e5b89d44cb5574684ae1fc641fdd7f71f3
Omar Bassuni <omarbassuni2@gmail.com>
2024-07-01 22:44:22 +0300

    Time: 40 ms (96.46%), Space: 48.7 MB (57.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,12 +2,17 @@
  * @param {number} n
  * @return {number}
  */
-const map = {
-    1: 1,
-    2: 2
-}
+// [0, 1, 2, 3, 4]
+// [0, 1, 2, 3, 5] 
+
+// The trick here is similiar to fib sequence, we add the last two numbers together
 var climbStairs = function(n) {
-    if(map[n]) return map[n];
-    map[n] = climbStairs(n - 1) + climbStairs(n - 2);
-    return map[n];
+    if(n <= 3)   return n;
+    let first = 0, second = 1, res;
+    for(let i = 1; i <= n; i += 1) {
+        res = first + second;
+        first = second;
+        second = res;
+    }
+    return res;
 };
