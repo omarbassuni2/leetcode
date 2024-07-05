9956aced1762d9993fd6ccc61a72da053699a3aa
Omar Bassuni <omarbassuni2@gmail.com>
2024-07-05 18:40:40 +0300

    Time: 42 ms (96.57%), Space: 49.1 MB (19.54%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -3,9 +3,14 @@
  * @param {number} n
  * @return {number}
  */
+/*
+    The trick here how to speed up the function and not exceed limits. So, if n % 2 === 0 we should call myPow with x*x and n/2
+    and If n < 0 we should return its inverse to be 1 divided by myPow(x, -n) => now we can get the power normally, and we reverse it after it finished
+*/
 var myPow = function(x, n) {
     if(n == 0)              return 1;
     else if(n < 0)          return 1 / myPow(x, -n);
     else if(n % 2 == 0)     return myPow(x * x, n / 2);
     else                    return x * myPow(x, n - 1);
+    
 };
