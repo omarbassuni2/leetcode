cf9d1c996abb0e84fbcad6dc5a24ca74f5d12541
Omar Bassuni <omarbassuni2@gmail.com>
2024-07-05 19:43:28 +0300

    Time: 83 ms (31.41%), Space: 54.3 MB (8.29%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -17,21 +17,20 @@
 [[0]], [[1]]
 */
 var setZeroes = function(matrix) {
-    const queue = [], top = 0, left = 0, bottom = matrix.length - 1, right = matrix[0].length - 1;
+    const left = 0, top = 0, right = matrix.length, bottom = matrix[0].length;
+    const zeros = [];
     matrix.forEach((row, i) => {
-        row.forEach((col, j) => {
-            if(col === 0)   queue.push([i, j]);
-        });
-    });
-    for(const cell of queue) {
-        let iIndex = cell[0], jIndex = cell[1];
-        // Go left to <= left
-        for(let j = jIndex; j >= left; j -= 1)    matrix[iIndex][j] = 0;
-        // Go right to >= right
-        for(let j = jIndex; j <= right; j += 1)    matrix[iIndex][j] = 0;
-        // Go bottom to >= bottom
-        for(let i = iIndex; i <= bottom; i += 1)    matrix[i][jIndex] = 0;
-        // Go top to <= top
-        for(let i = iIndex; i >= top; i -= 1)    matrix[i][jIndex] = 0;
+        row.forEach((value, j) => {
+            value === 0 && (zeros.push([i, j]));
+        })
+    })
+    for(const [i, j] of zeros) {
+        // rows
+        for(let k = i; k < right; k += 1)   matrix[k][j] = 0;
+        for(let k = i; k >= 0; k -= 1)      matrix[k][j] = 0;
+        
+        // cols
+        for(let k = j; k < bottom; k += 1)   matrix[i][k] = 0;
+        for(let k = j; k >= 0; k -= 1)      matrix[i][k] = 0;
     }
 };
