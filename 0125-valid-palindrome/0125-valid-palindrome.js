fa4263c0e7290d21bc98fabeb9f2fd2d18f5a25c
Omar Bassuni <omarbassuni2@gmail.com>
2024-07-20 05:47:36 +0300

    Time: 62 ms (65.38%), Space: 51.4 MB (82.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-    let left = 0, right = s.length -1;
-    while(left < right) {
-        if(s[left] !== s[right])    return false;
-        left += 1, right -= 1;
+    s = s.replaceAll(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let left = 0, right = s.length - 1;
+    while(left <= right) {
+        if(s[left] !== s[right])     return false;
+        right -= 1, left += 1;
     }
     return true;
 };
