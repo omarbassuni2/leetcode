46b58c9d58056a0e47d71fdb8c9b975b7e7793ce
Omar Bassuni <omarbassuni2@gmail.com>
2024-07-21 15:21:37 +0300

    Time: 117 ms (11.59%), Space: 56.1 MB (66.57%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -6,14 +6,15 @@ var sortedSquares = function(nums) {
     const output = [];
     let left = 0, right = nums.length - 1;
     while(left <= right) {
-        const leftSquare = Math.pow(nums[left], 2), rightSquare = Math.pow(nums[right], 2);
-        if(rightSquare >= leftSquare) {
-            output.unshift(rightSquare);
-            right -= 1;
-        } else {
-            output.unshift(leftSquare);
+        const leftElement = Math.pow(nums[left], 2), rightElement = Math.pow(nums[right], 2);
+        if(leftElement >= rightElement) {
+            output.unshift(leftElement);
             left += 1;
+        } else {
+            output.unshift(rightElement);
+            right -= 1;
         }
+        
     }
     return output;
 };
